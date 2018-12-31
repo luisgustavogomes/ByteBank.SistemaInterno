21decd4038f923e18aa8cd26cc38108ba29b3745
luisgustavogomes <luisgustavogomes@outlook.com>
2018-12-31 17:26:31 -0200

    Primeiro Commit

--- ByteBank.SistemaInterno/Program.cs
+++ ByteBank.SistemaInterno/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ByteBank.Modelos;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,11 @@ namespace ByteBank.SistemaInterno
     {
         static void Main(string[] args)
         {
+            ContaCorrente conta = new ContaCorrente(456, 456789);
+            Console.WriteLine("Número: " + conta.Numero + "\nAgencia: " + conta.Agencia + "\nSaldo: " + Format.FormatDouble(conta.Saldo));
+
+
+            Console.ReadLine();
         }
     }
 }
